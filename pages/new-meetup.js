95f712caad35974b5c6d9c8b4cf1d094358cf383
Nanok24 <ionmihneaandrei@gmail.com>
2023-03-28 02:15:41 +0300

    Added _head_ metadata in preparation for deployment

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,5 +1,7 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 import { useRouter } from "next/router";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function NewMeetupPage() {
   const router = useRouter();
@@ -20,7 +22,18 @@ function NewMeetupPage() {
     router.push("/");
   };
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create new friend gathering opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
